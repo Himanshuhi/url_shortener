0ea179d057ce7af3d6147368b32e054c8c988767
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-03-14 20:08:55 +0530

    Update URL shortening.py

--- URL shortening.py
+++ URL shortening.py
@@ -1,17 +1,13 @@
 import pyshorteners
 
-# Create an instance of the Shortener class
 shortener = pyshorteners.Shortener()
 
-# Shorten a URL using the TinyURL service
 short_url = shortener.tinyurl.short('https://google.com')
-
-# Shorten a URL using the Bit.ly service
+#you can use below code to
 #short_url = shortener.bitly.short('https://www.example.com')
 
-
-# Print the shortened URL
+#himanshu
 print(short_url)
 
 
-#pip install pyshorteners
+
